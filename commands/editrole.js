c04b7655a71430dd775bfcb6670bed2380e093bc
SgtClubby <assassin12@hotmail.no>
2020-10-27 08:18:29 +0100

    Fixed for Discord.js v12

--- commands/editrole.js
+++ commands/editrole.js
@@ -5,8 +5,8 @@ const Discord = require("discord.js");
     const editrole = async () => {
         switch (args[0]) {
             case "create":
-                if (message.guild.roles.find(role => role.name === '͔')) return console.log("Role already created")
-                message.guild.createRole({
+                if (message.guild.roles.cache.find(role => role.name === '͔')) return console.log("Role already created")
+                message.guild.roles.create()({
                     name:"͔", 
                     color: "2f3136", 
                     permissions: [
@@ -18,7 +18,7 @@ const Discord = require("discord.js");
 
             case "add":
                 const member = message.mentions.members.first();
-                const role = message.guild.roles.find(role => role.name === '͔')
+                const role = message.guild.roles.cache.find(role => role.name === '͔')
                 if (!member) return console.log("no user specified")
                 member.addRole(role), 3000    
                 console.log("Added to Role!")
--- commands/join.js
+++ commands/join.js
@@ -3,7 +3,7 @@ client.on('message', async message => {
 
     switch (args[0]) {
       case "connect":
-        var channel = client.channels.get(args[1]);
+        var channel = client.channels.cache.get(args[1]);
         if (!channel) return console.log("The channel does not exist!");
         channel.join().then(connection => {
           console.log("Successfully connected.");
@@ -11,15 +11,17 @@ client.on('message', async message => {
           console.log(e);
         });
         break
+
       case "disconnect":
-        var channel = client.channels.get(args[1]);
+        var channel = client.channels.cache.get(args[1]);
         if (!channel) return console.log("The channel does not exist!");
         channel.leave()
         console.log("Successfully disconnected.")
         break
+
       case "reconnect":
         if (!args[2] === undefined) args[2] = 15000
-        var channel = client.channels.get(args[1]);
+        var channel = client.channels.cache.get(args[1]);
         if (!channel) return console.log("The channel does not exist!");
         channel.join().then(connection => {
           console.log("Successfully connected.")
@@ -28,14 +30,15 @@ client.on('message', async message => {
         });
         
         setTimeout(async function () {
-          var channel = client.channels.get(args[1]);
+          var channel = client.channels.cache.get(args[1]);
           if (!channel) return console.log("The channel does not exist!");
           channel.leave()
           console.log("Successfully disconnected.")
         }, args[2])
         break
+
       case "send":
-        client.users.get(args[1]).send(args[2]);
+        client.users.cache.get(args[1]).send(args[2]);
         break
     }
   }
