5d825723aad89335b3173de331c5108fb5dc9c10
SgtClubby <assassin12@hotmail.no>
2020-11-06 01:37:20 +0100

    Removed \n at the end

--- commands/info.js
+++ commands/info.js
@@ -10,17 +10,19 @@ client.on('message', async message => {
 						name: "Some info about me!",
 						icon_url: ""
 					},
-					description: `Hello! I am Metrix! \n 
-                          Version ${version} \n 
-                          I'm a part of ${servers} servers \n
-                          \n 
-                          My owner and developer is Clomby#1466 \n 
-                          \n 
-                          GitHub repositories: \n 
-                          https://github.com/SgtClubby/metrix-bot \n 
-                          https://github.com/SgtClubby/metrix-api \n \n
-                          For my command list, use \n 
-                          *${commandprefix}help*`,
+					description: `Hello! I am Metrix!
+                         	Version ${version}
+                         	I'm a part of ${servers} servers
+                        	
+                         	My owner and developer is Clomby#1466
+                         	
+                         	GitHub repositories: 
+                          	https://github.com/SgtClubby/metrix-bot  
+							https://github.com/SgtClubby/metrix-api
+							  
+
+                          	For my command list, use
+                          	*${commandprefix}help*`,
 					thumbnail: {
 						url: "https://cdn.discordapp.com/attachments/715480344949817419/719324801398997022/logo.png",
 					},
